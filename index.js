b0cba1cf9ee8d1c790c3f801dd18f2fc476a4f18
motaz <motaz.dn.ali@gmail.com>
2023-03-16 22:55:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+
+const countDown = (number) => {
+  for (let i = number; i >= 0; i--) console.log(i);
+};
+
+const writeCards = (names) => {
+  const arr = [];
+  for (let i = 0; i < names.length; i++) {
+    arr.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return arr;
+};
